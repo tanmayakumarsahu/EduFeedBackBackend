b7b5065a1c8adc66fde21bde9696bbc2ef811c12
tanmayakumarsahu <tanmaya3230@gmail.com>
2026-04-28 13:02:19 +0530

    backend fix cors + auth working

---
+++ src/main/java/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.edufeedback.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5174",
+                                "https://edu-feedbackfrontend.vercel.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
