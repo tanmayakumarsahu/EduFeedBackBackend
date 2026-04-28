dcd1dffd0e9862cbda7fcc40eef05cafe8704460
Tanmaya Kumar Sahu <tanmaya3230@gmail.com>
2026-04-28 12:36:20 +0530

    Update User.java

--- src/main/java/com/edufeedback/backend/model/User.java
+++ src/main/java/com/edufeedback/backend/model/User.java
@@ -13,7 +13,7 @@ public class User {
 @GeneratedValue(strategy = GenerationType.IDENTITY)
 private Long id;
 
-@Column(nullable = false)
+@Column(nullable = false, unique = true)
 private String email;
 
 @Column(nullable = false)
@@ -70,4 +70,5 @@ public void setRole(String role) {
     this.role = role;
 }
 ```
+
 }
